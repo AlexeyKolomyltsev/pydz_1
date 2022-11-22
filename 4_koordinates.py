32a362ea54176442ec121ceb50cce92b609db29e
Alexey_wl <allex_doma@mail.ru>
2022-11-22 19:57:25 +0800

    добавил четвертую задачу

---
+++ 4_koordinates.py
@@ -0,0 +1,11 @@
+q_num = input("Введите номер четверти ")
+if q_num == "1":
+    print("x > 0 and y > 0")
+elif q_num == "2":
+    print("x < 0 and y > 0")
+elif q_num == "3":
+    print("x < 0 and y < 0")
+elif q_num == "4":
+    print("x > 0 and y < 0")
+else:
+    print("нет такой четверти")
